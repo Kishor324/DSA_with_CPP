44c22d2e44d85f6f32ea77cfaa26724d09db105c
Kishor324 <kishorbhatt157@gmail.com>
2026-03-15 20:50:58 +0545

    Traversal of singly linkedlist

---
+++ Linkedlist/traversal_singly.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// a linked list node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // constructor to initialize a new node with data
+    Node(int new_data) {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+// function to traverse and print the singly linked list
+void traverseList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data;
+        if (head->next != nullptr)  
+            cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+  
+    // create a hard-coded linked list:
+    // 10 -> 20 -> 30 -> 40
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    traverseList(head);
+
+    return 0;
+}
